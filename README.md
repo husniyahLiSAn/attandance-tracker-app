d900a10403b3f0cbfbdd3ac1c03c042ca2882097
Husniyah Lisan <husniyah.lisan@biofarma.co.id>
2023-09-16 23:17:48 +0700

    first commit features

--- README.md
+++ README.md
@@ -1,70 +1,55 @@
-# Getting Started with Create React App
+# Employee Attendance Tracker App
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+The Employee Attendance Tracker App is a web application built using [React](https://github.com/facebook/create-react-app). It allows you to manage employee attendance, including adding employee, clocking in and out, delete employee, and viewing their attendance history. The app demonstrates the use of classes, switch statements, and try-catch-finally statements for structured and error-handling code.
 
-## Available Scripts
+## Table of Contents
 
-In the project directory, you can run:
+- [Employee Attendance Tracker App](#employee-attendance-tracker-app)
+  - [Table of Contents](#table-of-contents)
+  - [Features](#features)
+  - [Validation](#validation)
+  - [Getting Started](#getting-started)
+  - [Usage](#usage)
+  - [Author](#author)
 
-### `npm start`
+## Features
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+The Employee Attendance Tracker App provides the following features, highlighting the use of classes, switch statements, and try-catch-finally statements:
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+1. **Employee Management**: You can add new employees, view their attendance history, and delete employees. The app leverages classes to manage employee objects and attendance records.
+   
+2. **Real-time Clocking**: The app displays the current time on the user interface, updating it every second through an interval implemented using `setInterval`.
 
-### `npm test`
+## Validation
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+The Employee Attendance Tracker App implemented using try-catch-finally statements to ensure data integrity and a smooth user experience.
 
-### `npm run build`
+- **Employee Name Validation**: When adding a new employee, the app checks if the name is required, has a minimum length of 4 characters, does not contain numbers, and is not a duplicate.
+    
+- **Attendance Actions Validation**: Marking attendance (Clock In or Clock Out) is carefully validated to prevent errors, and relevant error messages are displayed. The app ensures that clock-in is disabled if an employee has already clocked in and vice versa. 
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+## Getting Started
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+To get started with the Employee Attendance Tracker App, follow these steps:
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+1. Clone this repository to your local machine.
 
-### `npm run eject`
+2. Navigate to the project directory.
 
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
+3. Run `npm install` to install the necessary dependencies.
 
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+4. Run `npm start` to start the development server.
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
+5. Open your web browser and visit `http://localhost:3000` to use the app.
 
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
+## Usage
 
-## Learn More
+1. Enter the employee's name in the input field and click "Add" to add a new employee or can by enter. Ensure the name meets the validation criteria.
 
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
+2. To mark an employee's attendance, click the "Clock In" or "Clock Out" button in the table. The buttons are disabled as per the clock-in/clock-out status.
 
-To learn React, check out the [React documentation](https://reactjs.org/).
+3. To delete an employee from the list, click the "Delete" button in the table.
 
-### Code Splitting
+## Author
 
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+This Employee Attendance Tracker App was created by Lisa ([@HLyza4](https://twitter.com/HLyza4)). Feel free to reach out with any questions or feedback.
--- src/App.js
+++ src/App.js
@@ -1,25 +1,212 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      employees: [],
+      currentEmployee: '',
+      currentTime: '',
+      isClockInDisabled: false,
+      isClockOutDisabled: true,
+    };
+  }
+
+  componentDidMount() {
+    // Set an interval to update the current time every second
+    this.intervalID = setInterval(this.updateTime, 1000);
+  }
+
+  componentWillUnmount() {
+    // Clear the interval when the component is unmounted
+    clearInterval(this.intervalID);
+  }
+
+  // Function to update the current time
+  updateTime = () => {
+    const currentTime = new Date().toLocaleTimeString();
+    this.setState({ currentTime });
+  };
+
+  // Function to handle input field changes
+  handleInputChange = (event) => {
+    this.setState({ currentEmployee: event.target.value });
+  };
+
+  // Function to capitalize the first letter of a string
+  capitalizeFirstLetter = (str) => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  };
+
+  // Function to add a new employee
+  addEmployee = () => {
+    const { currentEmployee, employees } = this.state;
+
+    try {
+      // Validation: Check if the employee name is empty
+      if (currentEmployee.trim() === '') {
+        throw new Error("The employee name is required. It must be entered.");
+      }
+
+      // Validation: Check if the employee name has at least 4 characters
+      if (currentEmployee.trim().length < 4) {
+        throw new Error("The employee name must be at least 4 characters.");
+      }
+
+      // Validation: Check if the employee name contains numbers
+      if (/\d/.test(currentEmployee)) {
+        throw new Error("The employee name must not contain a number.");
+      }
+
+      // Validation: Check if the employee name already exists
+      if (employees.some((employee) => employee.name.toLowerCase() === currentEmployee.toLowerCase())) {
+        throw new Error("The employee name already exists.");
+      }
+
+      const newEmployee = {
+        id: employees.length + 1, // ID starts from 1
+        name: currentEmployee
+          .split(' ')
+          .map((word) => this.capitalizeFirstLetter(word))
+          .join(' '),
+        clockIn: '',
+        clockOut: '',
+        isClockInDisabled: false,
+        isClockOutDisabled: true,
+      };
+      employees.push(newEmployee);
+
+      this.setState({
+        employees,
+        currentEmployee: '',
+      });
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  // Function to mark attendance (Clock In or Clock Out)
+  markAttendance = (index, status) => {
+    const { employees, currentTime } = this.state;
+    const employee = employees[index];
+
+    try {
+      switch (status) {
+        case 'Clock In':
+          if (employee.isClockInDisabled) {
+            throw new Error("Employee has already clocked in.");
+          }
+          employee.clockIn = currentTime;
+          employee.isClockInDisabled = true;
+          employee.isClockOutDisabled = false;
+          break;
+        case 'Clock Out':
+          if (employee.isClockOutDisabled) {
+            throw new Error("Employee has already clocked out.");
+          }
+          employee.clockOut = currentTime;
+          employee.isClockInDisabled = true;
+          employee.isClockOutDisabled = true;
+          break;
+        default:
+          throw new Error("Invalid status.");
+      }
+
+      this.setState({
+        employees,
+      });
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  // Function to delete an employee
+  deleteEmployee = (index) => {
+    const { employees } = this.state;
+    employees.splice(index, 1);
+
+    this.setState({
+      employees,
+    });
+  };
+
+  render() {
+    const {
+      currentEmployee,
+      employees,
+      currentTime,
+    } = this.state;
+
+    return (
+      <div className="App">
+        <h1>Employee Attendance Tracker App</h1>
+        <div className="current-time">Current Time: {currentTime}</div>
+
+        <div className="container">
+          <div className="form">
+            <h2>Add Employee</h2>
+            <input
+              type="text"
+              placeholder="Employee Name"
+              value={currentEmployee}
+              onChange={this.handleInputChange}
+              onKeyDown={(e) => {
+                if (e.key === 'Enter') {
+                  this.addEmployee();
+                }
+              }}
+            />
+            <button onClick={this.addEmployee}>Add</button>
+          </div>
+        </div>
+
+        <div className="attendance-list">
+          <h2>Employee List</h2>
+          <table>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Employee Name</th>
+                <th>Clock In</th>
+                <th>Clock Out</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {employees.map((employee, index) => (
+                <tr key={employee.id}>
+                  <td>{employee.id}</td>
+                  <td>{employee.name}</td>
+                  <td>{employee.clockIn || 'Not Clocked In'}</td>
+                  <td>{employee.clockOut || 'Not Clocked Out'}</td>
+                  <td>
+                    <button
+                      onClick={() => this.markAttendance(index, 'Clock In')}
+                      disabled={employee.isClockInDisabled}
+                    >
+                      Clock In
+                    </button>
+                    <button
+                      onClick={() => this.markAttendance(index, 'Clock Out')}
+                      disabled={employee.isClockOutDisabled}
+                    >
+                      Clock Out
+                    </button>
+                    <button onClick={() => this.deleteEmployee(index)}>Delete</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+
+        <footer>
+          Created by Lisa - <a href="https://twitter.com/HLyza4" target="_blank" rel="noopener noreferrer">@HLyza4</a>
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
